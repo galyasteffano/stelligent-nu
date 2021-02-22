410f49ff51a6202d62b53429927327dec86dc495
Josh Dix <josh.dix@outlook.com>
2021-02-22 18:19:52 -0500

    wip

--- 01-cloudformation/lab-1.3.2/deploy.py
+++ 01-cloudformation/lab-1.3.2/deploy.py
@@ -19,14 +19,22 @@
 # fi
 
 import boto3
+from boto3 import session
 import json
 
 JSON_REGIONS_FILE = "/home/jdix/git_repos/stelligent-nu/01-cloudformation/lab-1.3.2/regions.json"
-
 with open(JSON_REGIONS_FILE, 'r') as conf_file:
-    regions = json.loads(conf_file.read())["Regions"]
+    REGIONS_TO_USE = json.loads(conf_file.read())["Regions"]
 
-for region in regions:
-    cloudformations_client = boto3.client('cloudformation')
-    print('hold')
-    cloudformations_client
+cf = session.client('cloudformation')
+
+def main():
+    for region in REGIONS_TO_USE:
+        cloudformations_client = boto3.client('cloudformation', region_name=region)
+        print('hold')
+        cloudformations_client.create_stack(json.loads(open('simple-s3.cfn.yaml')))
+
+
+
+if __name__ == '__main__':
+    main()
