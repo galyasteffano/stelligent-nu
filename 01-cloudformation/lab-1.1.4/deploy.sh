7dc91057f30e6f8c0bb9450414e05b7203e9452c
Josh Dix <josh.dix@outlook.com>
2021-02-22 12:59:55 -0500

    labs 1.2.x completed w/ retro

--- 01-cloudformation/lab-1.1.4/deploy.sh
+++ 01-cloudformation/lab-1.1.4/deploy.sh
@@ -2,7 +2,16 @@
 
 aws_sl='aws-vault exec stelligent_labs -- aws'
 
-# instructed to use 'same stack name' (don't think it matters)
-$aws_sl cloudformation deploy --template-file simple-s3.cfn.yaml --stack-name su-lab-1-1-4
+if [ -z $1 ];
+then
+    echo "using default region"
+    region_string=""
+else
+    echo "using set region $1"
+    region_string="--region $1"
+fi
+
+echo "$aws_sl cloudformation deploy --template-file simple-s3.cfn.yaml --stack-name su-lab-1-1-4 $region_string"
+$aws_sl cloudformation deploy --template-file simple-s3.cfn.yaml --stack-name su-lab-1-1-4 $region_string
 
 # $aws_sl cloudformation delete-stack --stack-name su-lab-1-1-4
